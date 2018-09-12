33918d1c5136e10ba0d98af78ceb39027c9e0e36
Erik <ethorelli@gmail.com>
2018-09-12 13:49:26 -0400

    create react-native-et.zsh and initial aliases

---
+++ custom/react-native-et.zsh
@@ -0,0 +1,8 @@
+# Aliases for additional ios simulators not already present in the react-native plugin that ships with oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh/tree/7c184fd0fc4a815c7185084099ec0f443b0dda56/plugins/react-native)
+
+alias rnios7='react-native run-ios --simulator "iPhone 7"'
+alias rnios7p='react-native run-ios --simulator "iPhone 7 Plus"'
+alias rnios8='react-native run-ios --simulator "iPhone 8"'
+alias rnios8p='react-native run-ios --simulator "iPhone 8 Plus"'
+alias rniosse='react-native run-ios --simulator "iPhone SE"'
+alias rniosx='react-native run-ios --simulator "iPhone X"'
